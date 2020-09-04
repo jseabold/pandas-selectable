7071c3f21e768adb9ad927b586a6c3e1b2a96068
Skipper Seabold <jsseabold@gmail.com>
2020-09-04 13:31:51 -0500

    Add metadata for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup
 
+
+with open("README.md") as readme:
+    description = readme.read()
+
+
 setup(
     name='pandas_selectable',
-    version='1.0',
+    version='1.0.1',
     description='Add a select accessor to pandas',
     packages=['pandas_selectable'],
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="Skipper Seabold",
+    url="https://github.com/jseabold/pandas-selectable",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires=">=3.6",
 )
